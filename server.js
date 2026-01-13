db703dfc4b15d4bf284a1608b99aa9c0dfa280ad
Vedant-kumar-27 <vedant.kumar2025@lpu.in>
2026-01-13 16:02:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("./config/db");
+
+// const authRoutes = require("./routes/authRoutes");
+// const vitalRoutes = require("./routes/vitalRoutes");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api", authRoutes);
+// app.use("/api", vitalRoutes);
+
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
+
 const express = require("express");
 const cors = require("cors");
 require("./config/db");
@@ -12,6 +30,9 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api", vitalRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
